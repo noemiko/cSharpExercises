eeb89ff2e06b5322b6629c89862d84f9f8f4c0c5
noemiko <noemikow8@gmail.com>
2017-03-01 20:11:36 +0100

    add class with main

---
+++ DelegateTimer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {    //define time to wake up
+            DateTime time = DateTime.Now.AddMilliseconds(5000);
+            Console.WriteLine("input: "+time);
+            //turn on alarm
+            Alarm budzik = new Alarm(time);
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
